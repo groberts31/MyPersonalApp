b4c40acd9e87179ecf298afa3ea1fb73b5f89f39
Greg Roberts <groberts3104@gmail.com>
2020-08-13 17:50:30 -0400

    added footer to page

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,30 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <Router>
       <div className="App">
-      <Wrapper>
-        <h1>Welcome To The Roberts Page!!!</h1>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Wrapper>
+          <h1>Welcome To The Roberts Page!!!</h1>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
         </Wrapper>
+        <Footer />
       </div>
     </Router>
   );
