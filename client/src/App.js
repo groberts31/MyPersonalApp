aba2ac749642abb768337aa6d139c21356c6842f
Greg Roberts <groberts3104@gmail.com>
2020-08-13 17:37:23 -0400

    added wrapper and router and installed react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
+import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
-    <div className="App">
-    <h1>Welcome To The Roberts Page!!!</h1>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+      <Wrapper>
+        <h1>Welcome To The Roberts Page!!!</h1>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        </Wrapper>
+      </div>
+    </Router>
   );
 }
 
