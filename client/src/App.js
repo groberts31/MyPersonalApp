cc3626b85c512008509175ecbf3d93c86e6e56c4
Greg Roberts <groberts3104@gmail.com>
2020-08-13 18:06:17 -0400

    changed logo to family pic

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import logo from "./logo.svg";
+import logo from "./family.png";
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Footer from "./components/Footer";
@@ -13,17 +13,6 @@ function App() {
           <h1>Welcome To The Roberts Page!!!</h1>
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
           </header>
         </Wrapper>
         <Footer />
